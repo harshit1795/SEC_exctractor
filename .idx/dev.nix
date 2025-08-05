4ab3a8fecd48954064c3c18947ff6a9d19806109
harshit.gola <harshit.gola@gmail.com>
2025-08-05 01:38:30 +0000

    Changes before Firebase Studio auto-run

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -6,11 +6,8 @@
 
   # Use https://search.nixos.org/packages to find packages
   packages = [
-    # pkgs.go
-    # pkgs.python311
-    # pkgs.python311Packages.pip
-    # pkgs.nodejs_20
-    # pkgs.nodePackages.nodemon
+    pkgs.python311
+    pkgs.python311Packages.pip
   ];
 
   # Sets environment variables in the workspace
@@ -25,16 +22,10 @@
     previews = {
       enable = true;
       previews = {
-        # web = {
-        #   # Example: run "npm run dev" with PORT set to IDX's defined port for previews,
-        #   # and show it in IDX's web preview panel
-        #   command = ["npm" "run" "dev"];
-        #   manager = "web";
-        #   env = {
-        #     # Environment variables to set for your server
-        #     PORT = "$PORT";
-        #   };
-        # };
+        web = {
+          command = ["streamlit" "run" "Home.py" "--server.port" "$PORT" "--server.headless" "true" "--server.enableCORS" "false"];
+          manager = "web";
+        };
       };
     };
 
@@ -42,8 +33,7 @@
     workspace = {
       # Runs when a workspace is first created
       onCreate = {
-        # Example: install JS dependencies from NPM
-        # npm-install = "npm install";
+        install-dependencies = "pip install -r requirements.txt";
       };
       # Runs when the workspace is (re)started
       onStart = {
